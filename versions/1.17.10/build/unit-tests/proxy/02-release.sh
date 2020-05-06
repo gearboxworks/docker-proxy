e81ead603d20898169a13be1e95e8734a030745c
MickMake <github@mickmake.com>
2020-05-06 20:57:33 +1000

    Rebuild with docker-template v1.6.0

---
+++ versions/1.17.10/build/unit-tests/proxy/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-05-06T20:55:39+1000, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "proxy-1.17.10" "Release test started."
+
+ps -eaf
+
+p_info "proxy-1.17.10" "Release test finished."
